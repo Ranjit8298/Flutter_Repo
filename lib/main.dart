a6cf5089ff21b894a6b3c9da0c22bbd882245aec
Ranjit8298 <ranjitkumarbgs61@gmail.com>
2023-01-16 18:05:02 +0530

    16 Jan, 2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_repo/Widgets/CustomButtonWidgets.dart';
+import 'package:flutter_repo/widgets/custom_textfield.dart';
+// import 'package:flutter_repo/Widgets/CustomButtonWidgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,8 +34,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var emailTxt = TextEditingController();
-  var passwordTxt = TextEditingController();
+  var numberTwo = TextEditingController();
+  var numberOne = TextEditingController();
+  var customTxt = TextEditingController();
+  var resultNumber = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -999,13 +1004,233 @@ class _MyHomePageState extends State<MyHomePage> {
       // ),
 
       //custom button call
-      body: CustomButtonWidgets(
-        btnName: 'Submit',
-        btnColor: Colors.red,
-        btnIcon: Icon(Icons.login_rounded),
-        callback: (() {
-          print('object clicked');
-        }),
+      // body: CustomButtonWidgets(
+      //   btnName: 'Submit',
+      //   btnColor: Colors.red,
+      //   btnIcon: Icon(Icons.login_rounded),
+      //   callback: (() {
+      //     print('object clicked');
+      //   }),
+      // ),
+
+      // Wrap Widgets
+      // body: Padding(
+      //   padding: const EdgeInsets.all(5.0),
+      //   child: Container(
+      //     width: double.infinity,
+      //     child: Wrap(
+      //       spacing: 10,
+      //       runSpacing: 10,
+      //       direction: Axis.horizontal,
+      //       alignment: WrapAlignment.center,
+      //       children: [
+      //       Container(
+      //         width: 100,
+      //         height: 100,
+      //         color: Colors.red,
+      //       ),
+      //       Container(
+      //         width: 100,
+      //         height: 100,
+      //         color: Colors.blue,
+      //       ),
+      //       Container(
+      //         width: 100,
+      //         height: 100,
+      //         color: Colors.black,
+      //       ),
+      //       Container(
+      //         width: 100,
+      //         height: 100,
+      //         color: Colors.green,
+      //       ),
+      //       Container(
+      //         width: 100,
+      //         height: 100,
+      //         color: Colors.yellow,
+      //       ),
+      //       Container(
+      //         width: 100,
+      //         height: 100,
+      //         color: Colors.orange,
+      //       ),
+      //       Container(
+      //         width: 100,
+      //         height: 100,
+      //         color: Colors.redAccent,
+      //       ),
+      //       Container(
+      //         width: 100,
+      //         height: 100,
+      //         color: Colors.purple,
+      //       ),
+      //       Container(
+      //         width: 100,
+      //         height: 100,
+      //         color: Colors.amber,
+      //       ),
+      //       Container(
+      //         width: 100,
+      //         height: 100,
+      //         color: Colors.pink,
+      //       ),
+      //       Container(
+      //         width: 100,
+      //         height: 100,
+      //         color: Colors.deepPurple,
+      //       ),
+      //       Container(
+      //         width: 100,
+      //         height: 100,
+      //         color: Colors.indigo,
+      //       ),
+      //     ]),
+      //   ),
+      // ),
+
+      // riched text
+      // body: RichText(
+      //     text: TextSpan(
+      //         style: TextStyle(color: Colors.grey, fontSize: 20),
+      //         children: <TextSpan>[
+      //       TextSpan(
+      //         text: 'Hello',
+      //       ),
+      //       TextSpan(text: 'World', style: TextStyle(color: Colors.blue, fontSize: 30))
+      //     ])),
+
+      // positioned widgets
+      // body: Container(
+      //   width: double.infinity,
+      //   height: double.infinity,
+      //   color: Colors.white10,
+      //   child: Stack(
+      //     children: [
+      //       Positioned(
+      //         right: -100,
+      //         top: -40,
+      //         child: Container(
+      //           width: 150,
+      //           height: 150,
+      //           decoration: BoxDecoration(
+      //             color: Colors.red.withOpacity(0.3),
+      //             borderRadius: BorderRadius.circular(75),
+      //           ),
+      //         ),
+      //       ),
+      //        Positioned(
+      //         right: -30,
+      //         top: -90,
+      //         child: Container(
+      //           width: 150,
+      //           height: 150,
+      //           decoration: BoxDecoration(
+      //             color: Colors.lightBlue.withOpacity(0.3),
+      //             borderRadius: BorderRadius.circular(75),
+      //           ),
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+
+      body: Container(
+        color: Colors.white24,
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.all(10),
+                child: TextField(
+                  controller: numberOne,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      hintText: 'Enter first Number',
+                      prefixIcon: const Icon(Icons.numbers_rounded),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: const BorderSide(color: Colors.grey))),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.all(10),
+                child: TextField(
+                  controller: numberTwo,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      hintText: 'Enter second Number',
+                      prefixIcon: const Icon(Icons.numbers_rounded),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: const BorderSide(color: Colors.grey))),
+                ),
+              ),
+              CustomTextField(
+                controllerTxt: customTxt,
+                keyboardType: TextInputType.emailAddress,
+                hintText: 'Enter Email',
+                labelText: 'Email',
+                prefixIcon: const Icon(Icons.date_range_rounded),
+                onChanged: (customTxt) {
+                  print(customTxt);
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton.icon(
+                      onPressed: () {
+                        var no1 = int.parse(numberOne.text.toString());
+                        var no2 = int.parse(numberTwo.text.toString());
+                        var sum = no1 + no2;
+                        resultNumber = "sum of $no1 and $no2 is: $sum";
+                        print(customTxt);
+                        setState(() {});
+                      },
+                      icon: const Icon(Icons.add),
+                      label: const Text('Add')),
+                  ElevatedButton.icon(
+                      onPressed: () {
+                        var no1 = int.parse(numberOne.text.toString());
+                        var no2 = int.parse(numberTwo.text.toString());
+                        var sub = no1 - no2;
+                        resultNumber = "substraction of $no1 and $no2 is: $sub";
+                        setState(() {});
+                      },
+                      icon: const Icon(Icons.minimize_rounded),
+                      label: const Text('Sub')),
+                  ElevatedButton.icon(
+                      onPressed: () {
+                        var no1 = int.parse(numberOne.text.toString());
+                        var no2 = int.parse(numberTwo.text.toString());
+                        var multi = no1 * no2;
+                        resultNumber =
+                            "multiplication of $no1 and $no2 is: $multi";
+                        setState(() {});
+                      },
+                      icon: const Icon(Icons.star_border_rounded),
+                      label: const Text('Multi')),
+                  ElevatedButton.icon(
+                      onPressed: () {
+                        var no1 = int.parse(numberOne.text.toString());
+                        var no2 = int.parse(numberTwo.text.toString());
+                        var div = no1 / no2;
+                        resultNumber = "division of $no1 and $no2 is: $div";
+                        setState(() {});
+                      },
+                      icon: const Icon(Icons.safety_divider_rounded),
+                      label: const Text('Div'))
+                ],
+              ),
+              Container(
+                  margin: const EdgeInsets.only(top: 20),
+                  child: Text(
+                    '$resultNumber',
+                    style: TextStyle(fontSize: 20),
+                  ))
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/widgets/custom_textfield.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controllerTxt;
+  final TextInputType keyboardType;
+  final String hintText;
+  final Widget prefixIcon;
+  final String labelText;
+  final Function onChanged;
+
+  const CustomTextField(
+      {super.key,
+      required this.controllerTxt,
+      required this.keyboardType,
+      required this.hintText,
+      required this.prefixIcon,
+      required this.labelText,
+      required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: TextField(
+        controller: controllerTxt,
+        keyboardType: keyboardType,
+        onChanged: onChanged(controllerTxt),
+        decoration: InputDecoration(
+          hintText: hintText,
+          prefixIcon: prefixIcon,
+          labelText: labelText,
+          border: const OutlineInputBorder(
+              // borderRadius: BorderRadius.circular(15),
+              // borderSide: const BorderSide(color: Colors.grey)
+              ),
+        ),
+      ),
+    );
+  }
+}
